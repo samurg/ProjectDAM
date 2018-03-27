42364ba4326893215a7dbe150233840e3e95bce9
samurg <samuel.rodriguezgonzalez@outlook.com>
2018-03-27 10:08:56 +0200

    firebase enviroment

--- application/src/app/app.module.ts
+++ application/src/app/app.module.ts
@@ -3,7 +3,10 @@ import { NgModule } from '@angular/core';
 
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
-
+import { environment } from '../environments/environment';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 @NgModule({
   declarations: [
@@ -11,8 +14,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    NgbModule.forRoot()
-  ],
+    NgbModule.forRoot(),
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
