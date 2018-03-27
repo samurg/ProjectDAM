9a0584ce42d782abb8875ac0c9c0973ce324d8f0
samurg <samuel.rodriguezgonzalez@outlook.com>
2018-03-27 10:00:06 +0200

    firebasedb service

--- application/src/environments/environment.ts
+++ application/src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyB_I4FVZRxP1V8jafjfIcsowslA4S6QCc0',
+    authDomain: 'proyectodam-259ec.firebaseapp.com',
+    databaseURL: 'https://proyectodam-259ec.firebaseio.com',
+    projectId: 'proyectodam-259ec',
+    storageBucket: 'proyectodam-259ec.appspot.com',
+    messagingSenderId: '1033203978735'
+  }
 };
