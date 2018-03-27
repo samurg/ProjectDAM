3ecb6ca07d950588d9f42226993bd116a34dd839
samurg <samuel.rodriguezgonzalez@outlook.com>
2018-03-27 11:44:31 +0200

    implementacion auth service

--- application/src/app/services/firebase/authentication/auth.service.ts
+++ application/src/app/services/firebase/authentication/auth.service.ts
@@ -1,8 +1,63 @@
 import { Injectable } from '@angular/core';
 
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+
+import { Observable } from 'rxjs/Observable';
+import { Router } from '@angular/router';
+
 @Injectable()
 export class AuthService {
+  user: Observable<firebase.User>;
+  constructor(private firebaseAuth: AngularFireAuth, private router: Router) {
+    this.user = firebaseAuth.authState;
+   }
 
-  constructor() { }
+   signup(email: string, password: string) {
+     this.firebaseAuth
+     .auth
+     .createUserWithEmailAndPassword(email, password)
+     .then(value => {
+       console.log('Usuario creado!! => ', value);
+       console.log('Su id es: ' + value.uid);
+     })
+     .catch(err => {
+        console.log('Algo ha fallado en singup => ', err.message);
+     });
+   }
+
+   login(email: string, password: string) {
+    this.firebaseAuth
+      .auth
+      .signInWithEmailAndPassword(email, password)
+      .then(value => {
+        console.log();
+        console.log('Nice, it worked!');
+        console.log('ID:' + value.uid);
+        /*this.router.navigate(['/proyects']);*/
+      })
+      .catch(err => {
+        console.log('Algo ha fallado en login => ', err.message);
+      });
+  }
+
+  logout() {
+    this.firebaseAuth
+      .auth
+      .signOut();
+  }
+
+  loginGoogle() {
+    const user = this.firebaseAuth
+    .auth
+    .signInWithPopup(new firebase.auth.GoogleAuthProvider()).then(value => {
+      console.log('Nice, it worked!');
+      /*this.router.navigate(['/proyects']);*/
+    })
+    .catch(err => {
+      console.log('Algo ha fallado en login google=> ', err.message);
+    });
+    console.log(user);
+  }
 
 }
