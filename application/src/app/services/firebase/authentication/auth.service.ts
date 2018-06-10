78e82a6764f0edf15a27688e773b650bc7191bab
samurg <samuel.rodriguezgonzalez@outlook.com>
2018-06-10 19:18:07 +0200

    comentado auth.service

--- application/src/app/services/firebase/authentication/auth.service.ts
+++ application/src/app/services/firebase/authentication/auth.service.ts
@@ -9,30 +9,43 @@ import { FbdbService } from '../database/fbdb.service';
 
 @Injectable()
 export class AuthService {
+
+  /* Usuario actual */
   user: Observable<firebase.User>;
+
   constructor(private _db: FbdbService, private firebaseAuth: AngularFireAuth, private router: Router) {
     firebaseAuth.authState.subscribe(user => {
       if (user) {
         this.user = firebaseAuth.authState;
       }
     });
-   }
+  }
 
-   signup(email: string, password: string) {
-     this.firebaseAuth
-     .auth
-     .createUserWithEmailAndPassword(email, password)
-     .then(value => {
-       this._db.increaseUsers();
-       this.user = this.firebaseAuth.authState;
-       this._db.registerUserFire(this.user);
-     })
-     .catch(err => {
+  /**
+   * Registra un usuario
+   * @param email
+   * @param password
+   */
+  signup(email: string, password: string) {
+    this.firebaseAuth
+      .auth
+      .createUserWithEmailAndPassword(email, password)
+      .then(value => {
+        this._db.increaseUsers();
+        this.user = this.firebaseAuth.authState;
+        this._db.registerUserFire(this.user);
+      })
+      .catch(err => {
         console.log('Algo ha fallado en singup => ', err.message);
-     });
-   }
+      });
+  }
 
-   login(email: string, password: string) {
+  /**
+   * Loguea un usuario
+   * @param email
+   * @param password
+   */
+  login(email: string, password: string) {
     this.firebaseAuth
       .auth
       .signInWithEmailAndPassword(email, password)
@@ -45,30 +58,36 @@ export class AuthService {
       });
   }
 
+  /**
+   * Realiza el signOut() del usuario actual
+   */
   logout() {
     this.firebaseAuth
       .auth
       .signOut();
-      this.user = this.firebaseAuth.authState;
+    this.user = this.firebaseAuth.authState;
   }
 
+  /**
+   * Loguea un usuario con google
+   */
   loginGoogle() {
     const user = this.firebaseAuth
-    .auth
-    .signInWithPopup(new firebase.auth.GoogleAuthProvider()).then(value => {
-      console.log('Nice, it worked!');
-      this.user = this.firebaseAuth.authState;
-      this.user.subscribe(e => {
-        this._db.getUserRegistered(e.uid).forEach(u => {
-          if (u.length === 0) {
-            this._db.registerUserFire(this.user);
-          }
+      .auth
+      .signInWithPopup(new firebase.auth.GoogleAuthProvider()).then(value => {
+        console.log('Nice, it worked!');
+        this.user = this.firebaseAuth.authState;
+        this.user.subscribe(e => {
+          this._db.getUserRegistered(e.uid).forEach(u => {
+            if (u.length === 0) {
+              this._db.registerUserFire(this.user);
+            }
+          });
         });
+        this.router.navigate(['projects']);
+      })
+      .catch(err => {
+        console.log('Algo ha fallado en login google=> ', err.message);
       });
-      this.router.navigate(['projects']);
-    })
-    .catch(err => {
-      console.log('Algo ha fallado en login google=> ', err.message);
-    });
   }
 }
